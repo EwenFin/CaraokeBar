3ab67237179a7f0f95ac8566e090d280799d3fe7
Ewen Carr <ewencarr@hotmail.com>
2017-02-10 11:08:18 +0000

    Guest class done

---
+++ week_02/weekend2_homework/guests.rb
@@ -0,0 +1,6 @@
+class Guest
+  attr_accessor :fav_song
+  def initialize(fav_song)
+    @fav_song = fav_song
+  end
+end
---
+++ week_02/weekend2_homework/specs/guests_spec.rb
@@ -0,0 +1,13 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../guests')
+
+class TestGuest < MiniTest::Test
+  def setup
+    @guest1 = Guest.new("9 to 5")
+  end
+
+  def test_guest_has_fav_song
+    assert_equal("9 to 5", @guest1.fav_song)
+  end
+end
